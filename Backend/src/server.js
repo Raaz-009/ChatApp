ce1a2c0ae48954841825f9e2ed2a012a0f79b310
Mohammad Thameem Raz <Mohammad.Thameem@novigo.com>
2026-01-20 11:03:56 +0530

    ready for deployement

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,25 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import path, { dirname } from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js"
 
 dotenv.config();
 console.log(process.env.PORT)
 
+const __dirname = path.resolve();
 const app =express();
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
+//make ready for deployment
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+}
+
+app.get("*", (_,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+});
 
 app.listen(process.env.PORT,()=> console.log("Server is running at port 3000"));
